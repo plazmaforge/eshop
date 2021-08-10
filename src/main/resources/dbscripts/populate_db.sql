25051abcaebdf483972ecaac11fdfd22ff3c4115
Oleh <ohapon@Olehs-MacBook-Pro.local>
2021-08-10 14:44:13 +0300

    Change DB scripts

--- src/main/resources/dbscripts/populate_db.sql
+++ src/main/resources/dbscripts/populate_db.sql
@@ -1,3 +1,3 @@
-INSERT INTO product (id, name, price, created_date) VALUES (100, 'Phone', 300.50, NULL);
-INSERT INTO product (id, name, price, created_date) VALUES (200, 'NoteBook', 700.75, NULL);
-INSERT INTO product (id, name, price, created_date) VALUES (300, 'TV', 900.20, NULL);
+INSERT INTO product (id, name, price, created_date) VALUES (100, 'Phone', 300.50, CURDATE());
+INSERT INTO product (id, name, price, created_date) VALUES (200, 'NoteBook', 700.75, CURDATE());
+INSERT INTO product (id, name, price, created_date) VALUES (300, 'TV', 900.20, CURDATE());
