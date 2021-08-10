431d65bad5af1836eae12711d92d6ea18689eccf
Oleh <ohapon@Olehs-MacBook-Pro.local>
2021-08-10 14:59:11 +0300

    Redesign

--- src/main/resources/dbscripts/populate_db.sql
+++ src/main/resources/dbscripts/populate_db.sql
@@ -1,3 +1,3 @@
 INSERT INTO product (id, name, price, created_date) VALUES (100, 'Phone', 300.50, CURDATE());
-INSERT INTO product (id, name, price, created_date) VALUES (200, 'NoteBook', 700.75, CURDATE());
-INSERT INTO product (id, name, price, created_date) VALUES (300, 'TV', 900.20, CURDATE());
+INSERT INTO product (id, name, price, created_date) VALUES (200, 'NoteBook', 1700.75, CURDATE());
+INSERT INTO product (id, name, price, created_date) VALUES (300, 'TV', 1200.20, CURDATE());
